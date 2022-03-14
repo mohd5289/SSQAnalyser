b7df8d28c80128691790955e2c20eefe5a727378
Osuolale <mohd5289@yahoo.com>
2022-03-14 04:48:49 +0100

    Changed Files

--- Downloads/SSQExpert-main-3/ssq_analyser/app/Models/Institution.php
+++ Downloads/SSQExpert-main-3/ssq_analyser/app/Models/Institution.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Institution extends Model
+class Institution extends Authenticable
 {
     use HasFactory;
     public function getSSQs()
